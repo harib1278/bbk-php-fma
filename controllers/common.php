18b3e79dc10bfb793bc2271eccb6579afa924f51
Thomas Shaddock <tomshaddock@googlemail.com>
2017-02-16 19:18:38 +0000

    small comment

--- controllers/common.php
+++ controllers/common.php
@@ -1,6 +1,6 @@
 <?php
 /**
-
+* Class was created to make the act of running a query as simple as instantiating an object and passing it a SQL query string on instantiation. 
 */
 
 class CommonController{
